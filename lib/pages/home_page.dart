879f4f5cfa382a84f2c60395c341f23ad4ea5f05
Prathamesh Rokade <prathameshr955@gmail.com>
2024-02-07 21:43:46 +0530

    Adding bottom nevigation bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,6 +8,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _currentPage = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,6 +44,41 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
+      bottomNavigationBar: _bottomNavigationBar(),
+    );
+  }
+
+  Widget _bottomNavigationBar() {
+    Color activeColor = Colors.red; // Color for the active icon
+    Color inactiveColor = Colors.grey; // Color for the inactive icon
+
+    return BottomNavigationBar(
+      currentIndex: _currentPage,
+      onTap: (_index) {
+        setState(() {
+          _currentPage = _index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          label: 'Feed',
+          icon: Icon(
+            Icons.feed,
+            color: _currentPage == 0
+                ? activeColor
+                : inactiveColor, // Change color based on active page
+          ),
+        ),
+        BottomNavigationBarItem(
+          label: 'Profile',
+          icon: Icon(
+            Icons.account_box,
+            color: _currentPage == 1
+                ? activeColor
+                : inactiveColor, // Change color based on active page
+          ),
+        ),
+      ],
     );
   }
 }
