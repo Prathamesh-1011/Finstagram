7cadcbeeeb323032303ae67f7a2fbfe0caf9511a
Prathamesh Rokade <prathameshr955@gmail.com>
2024-02-07 20:33:57 +0530

    selecting files from device storage

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,6 +1,8 @@
-// ignore_for_file: no_leading_underscores_for_local_identifiers, prefer_is_empty
+// ignore_for_file: no_leading_underscores_for_local_identifiers, prefer_is_empty, depend_on_referenced_packages, unused_field
 
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -17,6 +19,7 @@ class _RegisterPageState extends State<RegisterPage> {
   final GlobalKey<FormState> _registerFormKey = GlobalKey<FormState>();
 
   String? _name, _email, _password;
+  File? _image;
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +38,7 @@ class _RegisterPageState extends State<RegisterPage> {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 _titleWidget(),
+                _profileImageWidget(),
                 _registrationForm(),
                 _registerButton(),
               ],
@@ -69,6 +73,31 @@ class _RegisterPageState extends State<RegisterPage> {
             _emailTextField(),
             _passwordTextField(),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _profileImageWidget() {
+    var _imageProvider = _image != null
+        ? FileImage(_image!)
+        : const NetworkImage("https://i.pravatar.cc/300");
+    return GestureDetector(
+      onTap: () {
+        FilePicker.platform.pickFiles(type: FileType.image).then((_result) {
+          setState(() {
+            _image = File(_result!.files.first.path!);
+          });
+        });
+      },
+      child: Container(
+        height: _deviceHeight! * 0.15,
+        width: _deviceHeight! * 0.15,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: _imageProvider as ImageProvider,
+          ),
         ),
       ),
     );
